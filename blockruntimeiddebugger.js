3e9a9d4c3137cac6517f2c3edb55f6df547eece9
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-05-03 18:30:48 +0200

    Uploaded

---
+++ blockruntimeiddebugger.js
@@ -0,0 +1,45 @@
+/**
+* ░██████╗░██████╗░███████╗███████╗███╗░░██╗███████╗██████╗░░█████╗░░██████╗░
+* ██╔════╝░██╔══██╗██╔════╝██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗██╔════╝░
+* ██║░░██╗░██████╔╝█████╗░░█████╗░░██╔██╗██║█████╗░░██████╔╝██║░░██║██║░░██╗░
+* ██║░░╚██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██║░░██║██║░░╚██╗
+* ╚██████╔╝██║░░██║███████╗███████╗██║░╚███║██║░░░░░██║░░██║╚█████╔╝╚██████╔╝
+* ░╚═════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░╚═════╝░
+*
+* The content of this file is licensed using the CC-BY-4.0 license
+* which requires you to agree to its terms if you wish to use or make any changes to it.
+*
+* @license CC-BY-4.0
+* @link Github - https://github.com/andriycraft/GreenFrogMCBE
+* @link Discord - https://discord.gg/UFqrnAbqjP
+*/
+const Frog = require("../../src/Frog");
+
+const badIds = []
+
+module.exports = {
+  onLoad() {
+    Frog.eventEmitter.on('playerChat', handlePlayerChat);
+  }
+};
+
+function handlePlayerChat(event) {
+  const { player } = event;
+  const id = generateBlockId();
+
+  badIds.push(id)
+
+  player.sendMessage(`Generating block with id: ${id}`);
+  player.world.placeBlock(1059, 107, -908, id);
+}
+
+function generateBlockId() {
+  const min = 3000;
+  const max = 7999;
+
+  const id = Math.floor(Math.random() * (max - min + 1)) + min
+
+  if (badIds.includes(id)) generateBlockId();
+
+  return id;
+}
