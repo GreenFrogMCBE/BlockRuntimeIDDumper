f661c3e8e2d82536b26e49dba25d05aa67d6f44c
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-06-23 15:29:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,19 +15,9 @@
 */
 const Frog = require("../../src/Frog");
 
-const dumpedIds = [
-  339494639, 976881179, 994207970,
-  1481921079, 368734180, 555751865,
-  -1706049077, 927668178, -307481251,
-  1912846579, -336983999, -2080260717,
-  1014633093, 1576086683, 16603170,
-  904773342, 802520842, -976483655,
-  -362985564, 379907727, -156512822,
-  -2050534635, -1850412551, 769879805,
-  1313381298, -2106754178, -1037882073,
-  -1837802080, 556073414, -199522344,
-  -2089484049
-]
+const craftingDataToRuntimeIds = require('./craftingDataToRuntimeIds')
+
+const dumpedIds = craftingDataToRuntimeIds.generate()
 
 function handlePlayerChat(event) {
   const { player } = event;
